25482496e7c926ec3e55f8992858687cb3b9a245
ono <ono@ono>
2023-07-24 13:04:43 +0900

    added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,6 +48,17 @@
             this.button18 = new System.Windows.Forms.Button();
             this.button19 = new System.Windows.Forms.Button();
             this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
+            this.button25 = new System.Windows.Forms.Button();
+            this.button26 = new System.Windows.Forms.Button();
+            this.button27 = new System.Windows.Forms.Button();
+            this.button28 = new System.Windows.Forms.Button();
+            this.button29 = new System.Windows.Forms.Button();
+            this.button30 = new System.Windows.Forms.Button();
+            this.button31 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -250,11 +261,132 @@
             this.button20.UseVisualStyleBackColor = true;
             this.button20.Click += new System.EventHandler(this.button20_Click);
             // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(174, 70);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(75, 23);
+            this.button21.TabIndex = 20;
+            this.button21.Text = "button21";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(174, 99);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(75, 23);
+            this.button22.TabIndex = 21;
+            this.button22.Text = "button22";
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(174, 128);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(75, 23);
+            this.button23.TabIndex = 22;
+            this.button23.Text = "button23";
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(174, 157);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(75, 23);
+            this.button24.TabIndex = 23;
+            this.button24.Text = "button24";
+            this.button24.UseVisualStyleBackColor = true;
+            this.button24.Click += new System.EventHandler(this.button24_Click);
+            // 
+            // button25
+            // 
+            this.button25.Location = new System.Drawing.Point(174, 186);
+            this.button25.Name = "button25";
+            this.button25.Size = new System.Drawing.Size(75, 23);
+            this.button25.TabIndex = 24;
+            this.button25.Text = "button25";
+            this.button25.UseVisualStyleBackColor = true;
+            this.button25.Click += new System.EventHandler(this.button25_Click);
+            // 
+            // button26
+            // 
+            this.button26.Location = new System.Drawing.Point(174, 215);
+            this.button26.Name = "button26";
+            this.button26.Size = new System.Drawing.Size(75, 23);
+            this.button26.TabIndex = 25;
+            this.button26.Text = "button26";
+            this.button26.UseVisualStyleBackColor = true;
+            this.button26.Click += new System.EventHandler(this.button26_Click);
+            // 
+            // button27
+            // 
+            this.button27.Location = new System.Drawing.Point(174, 244);
+            this.button27.Name = "button27";
+            this.button27.Size = new System.Drawing.Size(75, 23);
+            this.button27.TabIndex = 26;
+            this.button27.Text = "button27";
+            this.button27.UseVisualStyleBackColor = true;
+            this.button27.Click += new System.EventHandler(this.button27_Click);
+            // 
+            // button28
+            // 
+            this.button28.Location = new System.Drawing.Point(174, 273);
+            this.button28.Name = "button28";
+            this.button28.Size = new System.Drawing.Size(75, 23);
+            this.button28.TabIndex = 27;
+            this.button28.Text = "button28";
+            this.button28.UseVisualStyleBackColor = true;
+            this.button28.Click += new System.EventHandler(this.button28_Click);
+            // 
+            // button29
+            // 
+            this.button29.Location = new System.Drawing.Point(174, 302);
+            this.button29.Name = "button29";
+            this.button29.Size = new System.Drawing.Size(75, 23);
+            this.button29.TabIndex = 28;
+            this.button29.Text = "button29";
+            this.button29.UseVisualStyleBackColor = true;
+            this.button29.Click += new System.EventHandler(this.button29_Click);
+            // 
+            // button30
+            // 
+            this.button30.Location = new System.Drawing.Point(12, 330);
+            this.button30.Name = "button30";
+            this.button30.Size = new System.Drawing.Size(75, 23);
+            this.button30.TabIndex = 29;
+            this.button30.Text = "yield";
+            this.button30.UseVisualStyleBackColor = true;
+            this.button30.Click += new System.EventHandler(this.button30_Click);
+            // 
+            // button31
+            // 
+            this.button31.Location = new System.Drawing.Point(12, 359);
+            this.button31.Name = "button31";
+            this.button31.Size = new System.Drawing.Size(156, 23);
+            this.button31.TabIndex = 30;
+            this.button31.Text = "union,except";
+            this.button31.UseVisualStyleBackColor = true;
+            this.button31.Click += new System.EventHandler(this.button31_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(261, 443);
+            this.Controls.Add(this.button31);
+            this.Controls.Add(this.button30);
+            this.Controls.Add(this.button29);
+            this.Controls.Add(this.button28);
+            this.Controls.Add(this.button27);
+            this.Controls.Add(this.button26);
+            this.Controls.Add(this.button25);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
             this.Controls.Add(this.button20);
             this.Controls.Add(this.button19);
             this.Controls.Add(this.button18);
@@ -303,6 +435,17 @@
         private System.Windows.Forms.Button button18;
         private System.Windows.Forms.Button button19;
         private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button25;
+        private System.Windows.Forms.Button button26;
+        private System.Windows.Forms.Button button27;
+        private System.Windows.Forms.Button button28;
+        private System.Windows.Forms.Button button29;
+        private System.Windows.Forms.Button button30;
+        private System.Windows.Forms.Button button31;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -36,6 +37,8 @@ namespace LinqBen
             {
                 Console.WriteLine(h.String());
             }
+            // こうすればよい
+            hoges.ForEach(x => Console.WriteLine(x.String()));
 
             var results = hoges.Where(x => x.Id == 1).Select(x => new Hoge(x));
             foreach (var h in results)
@@ -56,17 +59,19 @@ namespace LinqBen
         private void button2_Click(object sender, EventArgs e)
         {
             var nums = Enumerable.Range(1, 10);
-            foreach (var n in nums)
-            {
-                Console.WriteLine($"{n}");
-            }
+            //foreach (var n in nums)
+            //{
+            //    Console.WriteLine($"{n}");
+            //}
+            Console.WriteLine(nums.ToResult());
 
             // 0,1,2,3,4,
             nums = Enumerable.Range(0, 5);
-            foreach (var n in nums)
-            {
-                Console.WriteLine($"{n}");
-            }
+            //foreach (var n in nums)
+            //{
+            //    Console.WriteLine($"{n}");
+            //}
+            Console.WriteLine(nums.ToResult());
         }
 
         // 無名クラス
@@ -79,10 +84,11 @@ namespace LinqBen
                 return new { n = i, s = i == found_val ? "bingo" : "no" }; // 無名クラス ここでfound_valが使用可
             });
 
-            foreach (var elm in list)
-            {
-                Console.WriteLine($"{elm.n}:{elm.s}");
-            }
+            //foreach (var elm in list)
+            //{
+            //    Console.WriteLine($"{elm.n}:{elm.s}");
+            //}
+            Console.WriteLine(list.ToResult());
         }
 
         // 遅延実行
@@ -247,16 +253,22 @@ namespace LinqBen
                 new { Name="Ano", Score=50 },
             };
             Console.WriteLine("single key");
-            foreach (var elm in list.OrderBy(x => x.Score))
-            {
-                Console.WriteLine($"name:{elm.Name}, Score:{elm.Score}");
-            }
+            //foreach (var elm in list.OrderBy(x => x.Score))
+            //{
+            //    Console.WriteLine($"name:{elm.Name}, Score:{elm.Score}");
+            //}
+            Console.WriteLine(list.OrderBy(x => x.Score).ToResult());
+
             // 第2キー
             Console.WriteLine("double key");
-            foreach (var elm in list.OrderBy(x => x.Score).ThenBy(x => x.Name))
-            {
-                Console.WriteLine($"name:{elm.Name}, Score:{elm.Score}");
-            }
+            //foreach (var elm in list.OrderBy(x => x.Score).ThenBy(x => x.Name))
+            //{
+            //    Console.WriteLine($"name:{elm.Name}, Score:{elm.Score}");
+            //}
+            Console.WriteLine(list.OrderBy(x => x.Score).ThenBy(x => x.Name).ToResult());
+
+            var q = list.OrderBy(x => x.Score).ThenBy(x => x.Name);
+            Console.WriteLine(q.Select((a, i) => $"{i+1}:" + a.ToString()).ToResult());
         }
 
         private void button11_Click(object sender, EventArgs e)
@@ -279,18 +291,18 @@ namespace LinqBen
 
             var results = hoges.Select(x => x.Replace(" ", "").Replace(",", "")).Where(x => x.Length > 0);
             //var results = hoges.Where(x => x.Id == 1).Select(x => new Hoge(x));
-            foreach (var h in results)
-            {
-                Console.WriteLine(h);
-            }
-
+            //foreach (var h in results)
+            //{
+            //    Console.WriteLine(h);
+            //}
+            Console.WriteLine(results.ToResult());
         }
 
         // 数字列をカンマ区切り文字列に変換 sql で select * table where no in (1,2,3) などで使用
         private void button13_Click(object sender, EventArgs e)
         {
             var nums = Enumerable.Range(1, 5);
-            var connect_comma = string.Join((", "), nums.Select(x => x.ToString())); // "1, 2, 3, 4, 5"
+            var connect_comma = string.Join(", ", nums); // "1, 2, 3, 4, 5"
             Console.WriteLine(connect_comma);
         }
 
@@ -308,36 +320,6 @@ namespace LinqBen
 
         }
 
-        public class Hoge
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-
-            public Hoge() { }
-            public Hoge(Hoge x) { Id = x.Id; Name = x.Name + " copied"; }
-            public string String()
-            {
-                return $"{Id},{Name}";
-            }
-
-            // クラス選択してリファクタリングを選択すると、Equals の自動作成が選択可能になる
-            // 以下の２つはそれで作成された物
-            public override bool Equals(object obj)
-            {
-                var hoge = obj as Hoge;
-                return hoge != null &&
-                       Id == hoge.Id &&
-                       Name == hoge.Name;
-            }
-
-            public override int GetHashCode()
-            {
-                var hashCode = -1919740922;
-                hashCode = hashCode * -1521134295 + Id.GetHashCode();
-                hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
-                return hashCode;
-            }
-        }
 
         // distinct
         private void button15_Click(object sender, EventArgs e)
@@ -350,10 +332,12 @@ namespace LinqBen
         private void button16_Click_1(object sender, EventArgs e)
         {
             string aaa = "abc\n\rde";
-            aaa.Where(x => {
-                Console.WriteLine(x);
-                return true;
-            }).Count();
+            //aaa.Where(x => {
+            //    Console.WriteLine(x);
+            //    return true;
+            //}).Count();
+            // これでよかった
+            aaa.ToList().ForEach(x => Console.WriteLine(x));
 
             string new_aaa = new string(aaa.Where(x => !char.IsControl(x)).ToArray());
 
@@ -376,11 +360,6 @@ namespace LinqBen
             Console.WriteLine(henkan.FindIndex(x => x == 22)); // 4
         }
 
-        public class Hoge2
-        {
-            public int No { get; set; }
-            public string Name { get; set; }
-        }
         // distinct をクラスでやると？
         private void button18_Click(object sender, EventArgs e)
         {
@@ -408,18 +387,13 @@ namespace LinqBen
 
             smpl.Where(x => x.No == 3).First().Name = "onodesuyo";
 
-            foreach (var h in smpl)
-            {
-                Console.WriteLine($"No: {h.No}, Name: {h.Name}");
-            }
-
+            Console.WriteLine(smpl.ToResult());
+            //foreach (var h in smpl)
+            //{
+            //    Console.WriteLine($"No: {h.No}, Name: {h.Name}");
+            //}
         }
 
-        public class PetOwners
-        {
-            public string Name { get; set; }
-            public List<string> Pets { get; set; }
-        }
         // SelectMany 2段階のリストを平滑化する
         private void button20_Click(object sender, EventArgs e)
         {
@@ -441,6 +415,253 @@ namespace LinqBen
             {
                 Console.WriteLine($"{b.Name} has pets {b.Pet}");
             }
+            Console.WriteLine(q.ToResult());
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Enumerable.Range(1, 10).Count(v => { Console.Write($"{v},"); return true; } );
+            Console.WriteLine("");
+        }
+
+        // 無名クラスは、いい感じにToString()ができる。
+        // 通常のクラスは override しておくべし？
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(new { Name = "hoge", Ken = "Fukuoka", });
+            Console.WriteLine(new PetOwners { Name = "aaa", Pets = new List<string> { "neco", "inu", } });
+            Console.WriteLine(new Hoge2 { No = 4, Name = "toto" });
+        }
+
+        // GroupJoin (Outer Join)
+        private void button23_Click(object sender, EventArgs e)
+        {
+            var kens = new[]
+            {
+                new {No = 1, Name = "Aomori"},
+                new {No = 2, Name = "Iwate"},
+                new {No = 3, Name = "Fukuoka"},
+            };
+            var adrs = new[]
+            {
+                new { Simei = "Ono Taka", KenCode = 2, },
+                new { Simei = "Tanaka Hoge", KenCode = 0, },
+                new { Simei = "Sato Ken", KenCode = 3, },
+            };
+
+            // Left Outer Join 風にできる
+            // 1:右のテーブル
+            // 2:左テーブルのキー
+            // 3:右テーブルのキー
+            // 4:第1が左のレコード、第2が右のレコード群（0個もあり）
+            Console.WriteLine(adrs.GroupJoin(
+                kens,
+                x => x.KenCode,
+                x => x.No,
+                (a, b) => new { Simei = a.Simei, KenMei = string.Join("", b.Select(x => x.Name)), }
+            ).ToResult());
+        }
+
+        // GroupBy そのまま
+        private void button24_Click(object sender, EventArgs e)
+        {
+            var adrs = new[]
+            {
+                new { Simei = "Ono Taka", KenCode = 2, },
+                new { Simei = "Tanaka Hoge", KenCode = 1, },
+                new { Simei = "Sato Ken", KenCode = 3, },
+                new { Simei = "Ogoori No", KenCode = 2, },
+            };
+            var kens = new[]
+            {
+                new {No = 1, Name = "Aomori"},
+                new {No = 2, Name = "Iwate"},
+                new {No = 3, Name = "Fukuoka"},
+            };
+
+            // 1: キー
+            // 2: 第1がキー,第2が該当リスト
+            var q = adrs.GroupBy(
+                x => x.KenCode,
+                (ken, lt) => $"[" + kens.FirstOrDefault(x => x.No == ken).Name + "- {" + string.Join(",", lt.Select(x => x.Simei)) + "}]"
+            );
+            Console.WriteLine(q.ToResult());
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            // FirstOrDefault で、ない場合はnewしてくれるのか？
+            var hogeList = new List<Hoge> {
+                new Hoge { Id=1, Name="aa", },
+                new Hoge { Id=2, Name="bb", },
+                new Hoge { Id=3, Name="cc", },
+            };
+            var q = hogeList.FirstOrDefault(x => x.Id == 2);
+            Console.WriteLine(q.ToString());
+            var q2 = hogeList.FirstOrDefault(x => x.Id == 22);
+            
+            Console.WriteLine(q2.ToString());
+            // ANSWER：　class の場合は null となる！
+
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            var lt = new List<int>{ 1, 2, 3, 4, 5, };
+            lt.ForEach(x => Console.WriteLine(x));
+
+
+            // reverse は破壊的。コピーしてからしないと元が変更される。
+            var lt_r = new List<int>(lt);
+            lt_r.Reverse();
+            lt.ForEach(x => Console.WriteLine(x));  // 1,2,3,4,5
+            lt_r.ForEach(x => Console.WriteLine(x));// 5,4,3,2,1
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            // insert
+            var lt = new List<int>();
+            lt.Insert(0, 1); // 空でも挿入できる
+            lt.Insert(0, 2); // 先頭に挿入
+            lt.Insert(lt.Count(), 3); // 最後に挿入（追加と同じ）
+
+            lt.ForEach(x => Console.WriteLine(x));
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            // ソートの時間を計測
+            const int CNT = 10000; // 1万件 
+            var lst = new List<int>();
+            var rnd = new Random();
+            foreach (var n in Enumerable.Range(0, CNT))
+            {
+                lst.Add(rnd.Next(CNT));
+            }
+
+            Console.WriteLine(lst.Count());
+
+            var sw = new Stopwatch();
+            sw.Restart();
+            var lst2 = lst.OrderBy(x => x).ToList();
+            Console.WriteLine($"sorting span = {sw.ElapsedMilliseconds}[ms]");
+
+            Console.WriteLine(lst.Count());
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            var hogeList = new List<Hoge> {
+                new Hoge { Id=1, Name="aa", },
+                new Hoge { Id=2, Name="bb", },
+                new Hoge { Id=3, Name="cc", },
+                new Hoge { Id=1, Name="aaa", },
+                new Hoge { Id=3, Name="dd", },
+            };
+
+            var hogeList2 = new List<Hoge>(hogeList);
+            hogeList2.Reverse();
+            //hogeList2.ForEach(x => Console.WriteLine(x.String()));
+
+
+            // OrderdDictionary   ポイント：key のオーダー順で入っている
+            var dic = new SortedDictionary<int, string>();
+            hogeList2.ForEach(x => {
+                if (!dic.ContainsKey(x.Id)) dic.Add(x.Id, x.Name);
+            });
+            foreach(var elm in dic)
+            {
+                Console.WriteLine($"key:{elm.Key}, val:{elm.Value}");
+            }
+        }
+
+        // yieldって？イェルドという発音らしい
+        private void button30_Click(object sender, EventArgs e)
+        {
+            var l = yield_sample();
+            Console.WriteLine(l.ToResult());
+
+            var l2 = yield_sample_chien();
+            Console.WriteLine(l2.Take(5).ToResult()); // 先頭の５つだけ
+        }
+        static IEnumerable<int> yield_sample()
+        {
+            for (var i =0; i < 10; ++i) { yield return i; }
+        }
+        // 遅延評価？なのでこんなことも可能
+        static IEnumerable<int> yield_sample_chien()
+        {
+            int i = 0;
+            while (true) { yield return i++; }
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            var a = new List<int> { 10, 20, 30, 40, 50, 10, };
+            var b = new List<int> { 15, 25, 30, };
+            var nl = a.Union(b); // 同じ物は入らない（a,bの両方とも）
+            Console.WriteLine(nl.ToResult());
+
+            var nl2 = a.Except(b); // 自分の中の重複もなくなる。aの中に無いものを引いてもよい。
+            Console.WriteLine(nl2.ToResult());
+        }
+    }
+    public class Hoge
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Hoge() { }
+        public Hoge(Hoge x) { Id = x.Id; Name = x.Name + " copied"; }
+        public string String()
+        {
+            return $"{Id},{Name}";
+        }
+
+        // クラス選択してリファクタリングを選択すると、Equals の自動作成が選択可能になる
+        // 以下の２つはそれで作成された物
+        public override bool Equals(object obj)
+        {
+            var hoge = obj as Hoge;
+            return hoge != null &&
+                   Id == hoge.Id &&
+                   Name == hoge.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1919740922;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            return hashCode;
+        }
+    }
+
+    public class Hoge2
+    {
+        public int No { get; set; }
+        public string Name { get; set; }
+        override public string ToString()
+        {
+            return $"{{ No={No}, Name={Name} }}";
+        }
+    }
+    public class PetOwners
+    {
+        public string Name { get; set; }
+        public List<string> Pets { get; set; }
+        public override string ToString()
+        {
+            return $"{{ Name={Name}, Pets={{ {string.Join(",", Pets)} }} }}";
+        }
+    }
+
+    public static class MyExtendMethod
+    {
+        public static string ToResult<T>(this System.Collections.Generic.IEnumerable<T> source)
+        {
+            return $"{{ {string.Join("," + Environment.NewLine, source)} }}";
         }
     }
 }
